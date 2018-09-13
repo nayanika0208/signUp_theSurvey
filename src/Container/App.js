5441a3c38efd1214e89eef3db4911a24fd63391e
nayanika0208 <nayanika.0208@gmail.com>
2018-09-13 15:21:00 +0530

    added footer

---
+++ src/Container/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+import Footer from '../Components/Footer/footer';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Footer/>
+      </div>
+    );
+  }
+}
+
+export default App;
